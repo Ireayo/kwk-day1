0122e17996b1468f9e7e18deb0064700169d8be8
Apple <apple@Apples-MacBook-Pro-2.local>
2019-07-22 12:28:42 -0700

    keep going

---
+++ 01 datatypes.playground/Contents.swift
@@ -0,0 +1,28 @@
+var first = "KarlieKloss"
+var second = "KarlieKlie"
+var third = "Karliekloss"
+var fourth = "Kloss Karlie Kloss Karlie"
+var fifth = " I love Karlie"
+
+/* another statement*/
+var integer = 4.5
+var double = 4.6
+print(integer*double)
+
+
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+var average = (a+b+c+d)/4
+var  incomeperhour = 10.25
+var hourslastweek = 20.0
+incomeperhour*hourslastweek
+
+
+
+
+
+
+
---
+++ 02_conditionals.playground/Contents.swift
@@ -0,0 +1,39 @@
+//intention this playground test dfferent comparison//import UIKit
+
+5<3
+7 <= 6
+"busy" == "busy"
+16 == 18-4
+// use = to set a value to something
+//use == to check if two things are equal
+
+
+
+// check what && does
+12 == 10+2 && 3 == 1+2
+12 == 10+2 && 3 == 1+8
+//&& checks if both sides are true if not false will return
+
+
+// check what does ||
+
+ 13 == 12+1 || 12 == 1+1 // left side true but right is false
+/*
+var dogAge = 12
+if dogAge < 2 { print (" you are a puppy 🐶")}
+else    {print(" You are elderly ")}
+*/
+
+
+var favoriteFood = "cupcakes"
+if favoriteFood == "chipotle" {
+    print ("healthy choice")
+}
+else {
+        print("bad choice ")
+}
+/*
+    print ("healthy choice")}
+else  { print " }
+*/
+
